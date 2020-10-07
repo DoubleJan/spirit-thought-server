edd79dff9b7f71b69855b905b91ee0e9cb9766c3
doublejan <954464727@qq.com>
2020-10-07 18:28:41 +0800

    feat: 添加生产环境静态资源地址

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -2,6 +2,6 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
-  // config.fileRoot = 'c://files/spirit-thought-server/assets';
+  config.fileRoot = '/opt/app/files';
   return config;
 };
